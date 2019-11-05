67b745afaaad186065d87def66082107e264de06
Laura <laura.vivas.gamboa@gmail.com>
2019-11-05 13:54:05 +0100

    Infinite growth burguer

--- js/game.js
+++ js/game.js
@@ -3,6 +3,8 @@ const Game = {
     ctx: undefined,
     width: undefined,
     height: undefined,
+    minBurguerSizeX: 180,
+    maxBurguerSizeX: 375,
     fps: 60,
     framesCounter: 0,
     score: 0,
@@ -29,8 +31,12 @@ const Game = {
             this.clear();
             this.drawAll();
             this.moveAll();
-            if (this.framesCounter % 70 === 0) this.generateObstacles();
-            if (this.framesCounter % 80 === 0) this.generatePrices();
+            if (this.framesCounter % 90 === 0) this.generateObstacles();
+            if (this.framesCounter % 95 === 0) this.generatePrices();
+            this.clearObstacles();
+            this.clearPrices();
+           // this.isCollisionObs();
+            this.isCollisionPrice();
 
         }, 1000 / this.fps)
     },
@@ -41,6 +47,8 @@ const Game = {
         this.player.setListeners();
         this.obstacles = [];
         this.prices = [];
+
+
     },
 
     clear: function () {
@@ -59,7 +67,6 @@ const Game = {
     moveAll: function () {
         this.background.move();
         this.player.move();
-        console.log(this.obstacles)
         this.obstacles.forEach(obstacles => obstacles.move())
         this.prices.forEach(prices => prices.move())
     },
@@ -70,8 +77,52 @@ const Game = {
 
 
     generatePrices: function () {
-        this.obstacles.push(new Prices(this.ctx, 70, 45, this.width, this.height));
+        this.prices.push(new Prices(this.ctx, 70, 45, this.width, this.height));
     },
 
+    goSmallBurguer: function () {
+        this.player.width *= 0.99
+        this.player.height *= 0.99
+    },
 
+    isCollisionObs: function () {
+        this.obstacles.forEach(obstacle => {
+            if ((this.player.posX + this.player.width > obstacle.posX &&
+                    this.player.posX < obstacle.posX + obstacle.width &&
+                    obstacle.posY < this.player.posY + this.player.height &&
+                    obstacle.posY + obstacle.height > this.player.posY)) {
+                this.goSmallBurguer()
+            }
+            // else if (this.minBurguerSizeX > this.player.width) {
+            //     alert("FRED IS ON A DIET NOW");
+            // }
+        })
+    },
+
+    goBigBurguer: function () {
+        this.player.width *= 1.01
+        this.player.height *= 1.01
+    },
+
+    isCollisionPrice: function () {
+        this.prices.forEach(price => {
+            if ((this.player.posX + this.player.width > price.posX &&
+                this.player.posX < price.posX + price.width &&
+                price.posY < this.player.posY + this.player.height &&
+                price.posY + price.height > this.player.posY)) {
+                this.goBigBurguer()
+            } 
+            // else if (this.maxBurguerSizeY <= this.player.height) {
+            //     alert("FRED IS DEAD");
+            // }
+        })
+    },
+
+    clearObstacles() {
+        this.obstacles = this.obstacles.filter(obstacle => obstacle.posX > 0);
+    },
+
+    clearPrices() {
+        this.prices = this.prices.filter(price => price.posX > 0);
+    },
 }
